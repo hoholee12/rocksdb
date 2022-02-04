88bc3cefca527e0bc34f4e36eeca4300c74238f6
hoholee12 <hoholee12@gmail.com>
2022-02-04 00:51:01 -0800

    update bench

--- benchmark.sh
+++ benchmark.sh
@@ -4,10 +4,11 @@ if [[ $1 == "init" ]]; then
 rm -rf /tmp/rocksdbtest-*
 ./db_bench -benchmarks=fillrandom -write_buffer_size=1000 -target_file_size_base=1000 -num=100000
 else
-uftrace ./db_bench -benchmarks=updaterandom -write_buffer_size=1000 -target_file_size_base=1000 -num=1000 -use_existing_db=false > test.txt
+uftrace ./db_bench -benchmarks=updaterandom -write_buffer_size=1000 -target_file_size_base=1000 -num=1000 -use_existing_db=true > test.txt
 
 rm -rf thread*.c
 x=-1
+unset thread_list
 for i in $(cat test.txt | sed -n 's/.*\[\(.*[0-9]\)\].*/\1/p'); do
 	if [[ ! ${thread_list[*]} =~ $i ]]; then
 		x=$(($x+1))
