c7c8a1b80552beebbed47f4f48b5a31047e80f80
hoholee12 <hoholee12@gmail.com>
2022-02-16 14:57:26 +0900

    aging test

---
+++ run_bench_age.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo uniform workload
+echo dataset $1
+
+if [[ $6 == "init" ]]; then
+	sudo time ./db_bench_"$3" \
+	 -benchmarks="fillrandom" \
+	 -num=$1 \
+	 -threads=1 \
+	 -max_background_jobs=8 \
+	 -histogram \
+	 -statistics \
+	 -use_existing_db=false \
+	 -db=/home/jeongho/mnt \
+	 -use_direct_io_for_flush_and_compaction=false \
+	 -use_direct_reads=false \
+	 
+	echo after run...
+	df -T | grep mnt
+
+else
+	#run
+	sudo time ./db_bench_"$3" \
+	 -benchmarks="updaterandom" \
+	 -num=$1 \
+	 -threads=1 \
+	 -max_background_jobs=8 \
+	 -histogram \
+	 -statistics \
+	 -use_existing_db=true \
+	 -db=/home/jeongho/mnt \
+	 -use_direct_io_for_flush_and_compaction=false \
+	 -use_direct_reads=false \
+	 
+	echo after run...
+	df -T | grep mnt
+
+fi
