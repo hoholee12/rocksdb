64c1b6c57c9937238166f69ef6d4deed2c77c655
hoholee12 <hoholee12@gmail.com>
2022-02-17 00:22:10 -0800

    wtf

--- installtoycsb.sh
+++ installtoycsb.sh
@@ -3,7 +3,7 @@
 sudo make rocksdbjavastaticrelease -j3
 cd ../YCSB
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-mvn -pl com.yahoo.ycsb:rocksdb-binding -am clean package
+mvn -pl site.ycsb:rocksdb-binding -am clean package
 cd ../rocksdb
 cp java/target/rocksdbjni-6.24.0.jar ~/.m2/repository/org/rocksdb/rocksdbjni/6.2.2/rocksdbjni-6.2.2.jar
 cp java/target/rocksdbjni-6.24.0.jar ../YCSB/rocksdb/target/dependency/rocksdbjni-6.2.2.jar
