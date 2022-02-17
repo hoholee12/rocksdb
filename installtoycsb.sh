3203cbe6f8f8edcb027f07e976f2b2a2562641eb
hoholee12 <hoholee12@gmail.com>
2022-02-16 20:53:32 -0800

    update ycsb

--- installtoycsb.sh
+++ installtoycsb.sh
@@ -5,4 +5,5 @@ echo "i assume that youve already compiled ycsb."
 cp java/target/rocksdbjni-6.24.0.jar ~/.m2/repository/org/rocksdb/rocksdbjni/6.2.2/rocksdbjni-6.2.2.jar
 cp java/target/rocksdbjni-6.24.0.jar ../YCSB/rocksdb/target/dependency/rocksdbjni-6.2.2.jar
 echo "just a little test."
-../YCSB/bin/ycsb load rocksdb -s -P workloads/workloadc -p rocksdb.dir=/tmp/ycsb-rocksdb-data
+cd ../YCSB
+./bin/ycsb load rocksdb -s -P workloads/workloada -p rocksdb.dir=/tmp/ycsb-rocksdb-data
