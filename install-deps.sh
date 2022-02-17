6b3019ca673f8a722ae836fa598daabaeab2bf2f
hoholee12 <hoholee12@gmail.com>
2022-02-17 17:13:43 +0900

    update

--- install-deps.sh
+++ install-deps.sh
@@ -3,4 +3,4 @@
 
 sudo apt-get install libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev libzstd-dev
 #for compiling rocksdbjava
-sudo apt-get install curl openjdk-8-jdk
+sudo apt-get install curl openjdk-8-jdk maven
