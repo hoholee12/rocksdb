5688e205ebedc00d5755d1795833850581e056de
hoholee12 <hoholee12@gmail.com>
2022-02-17 17:42:04 +0900

    fix

--- runycsb.sh
+++ runycsb.sh
@@ -14,5 +14,8 @@ if [[ $locationhere == "" ]]; then
 	locationhere="/home/jeongho/mnt/ycsbtest"
 fi
 
+cp java/target/rocksdbjni-6.24.0.jar /home/jeongho/.m2/repository/org/rocksdb/rocksdbjni/6.2.2/rocksdbjni-6.2.2.jar
+cp java/target/rocksdbjni-6.24.0.jar /root/.m2/repository/org/rocksdb/rocksdbjni/6.2.2/rocksdbjni-6.2.2.jar
+cp java/target/rocksdbjni-6.24.0.jar /home/jeongho/YCSB/rocksdb/target/dependency/rocksdbjni-6.2.2.jar
 cd ../YCSB
 ./bin/ycsb load rocksdb -s -P workloads/$workloadhere -p recordcount=$counthere -p operationcount=$counthere -p rocksdb.dir=$locationhere
