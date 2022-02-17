59631a4dbeddb4bd89873790f9ad8a77c27c2149
hoholee12 <hoholee12@gmail.com>
2022-02-17 17:48:28 +0900

    fix

--- runycsb.sh
+++ runycsb.sh
@@ -3,6 +3,7 @@
 workloadhere=$1
 counthere=$2
 locationhere=$3
+typehere=$4
 
 if [[ $workloadhere == "" ]]; then
 	workloadhere="workloadc"
@@ -13,12 +14,18 @@ fi
 if [[ $locationhere == "" ]]; then
 	locationhere="/home/jeongho/mnt/ycsbtest"
 fi
+if [[ $typehere == "" ]]; then
+	typehere="load"
+fi
 
 cp java/target/rocksdbjni-6.24.0.jar /home/jeongho/.m2/repository/org/rocksdb/rocksdbjni/6.2.2/rocksdbjni-6.2.2.jar
 cp java/target/rocksdbjni-6.24.0.jar /root/.m2/repository/org/rocksdb/rocksdbjni/6.2.2/rocksdbjni-6.2.2.jar
 cp java/target/rocksdbjni-6.24.0.jar /home/jeongho/YCSB/rocksdb/target/dependency/rocksdbjni-6.2.2.jar
 cd ../YCSB
-echo "load..."
-./bin/ycsb load rocksdb -s -P workloads/$workloadhere -p recordcount=$counthere -p operationcount=$counthere -p rocksdb.dir=$locationhere
-echo "run..."
-./bin/ycsb run rocksdb -s -P workloads/$workloadhere -p recordcount=$counthere -p operationcount=$counthere -p rocksdb.dir=$locationhere
+if [[ $typehere == "load" ]]; then
+	echo "load..."
+	./bin/ycsb load rocksdb -s -P workloads/$workloadhere -p recordcount=$counthere -p operationcount=$counthere -p rocksdb.dir=$locationhere
+else
+	echo "run..."
+	./bin/ycsb run rocksdb -s -P workloads/$workloadhere -p recordcount=$counthere -p operationcount=$counthere -p rocksdb.dir=$locationhere
+fi
